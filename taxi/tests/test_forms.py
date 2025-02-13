a050365bd6fc73b153480700c02d68ba416c288a
Andrii Fedchuk <andriyfedchuk2006@gmail.com>
2025-02-13 17:35:23 +0100

    Solution

---
+++ taxi/tests/test_forms.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from django.urls import reverse
+
+from taxi.forms import CarSearchForm
+from taxi.models import Manufacturer, Car
+
+
+class CarSearchFormTests(TestCase):
+    def setUp(self):
+        self.manufacturer = Manufacturer.objects.create(name="Test", country="Test")
+        self.car1 = Car.objects.create(model="Test1", manufacturer=self.manufacturer)
+        self.car2 = Car.objects.create(model="Test2", manufacturer=self.manufacturer)
+
+    def test_search_form_display(self):
+        response = self.client.get(reverse("taxi:car-list"))
+        self.assertEqual(response.status_code, 200)
---
+++ taxi/tests/test_models.py
@@ -0,0 +1,26 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+from taxi.models import Manufacturer, Car
+
+
+class ModelsTests(TestCase):
+    def test_manufacturer_str(self):
+        manufacturer = Manufacturer.objects.create(name="Test", country="Ukraine")
+        self.assertEqual(str(manufacturer), f"{manufacturer.name} {manufacturer.country}")
+
+    def test_driver_str(self):
+        driver = get_user_model().objects.create(
+            username="test",
+            first_name="test_first_name",
+            last_name="test_last_name",
+        )
+        self.assertEqual(
+            str(driver),
+            f"{driver.username} ({driver.first_name} {driver.last_name})"
+        )
+
+    def test_car_str(self):
+        manufacturer = Manufacturer.objects.create(name="Test", country="Ukraine")
+        car = Car.objects.create(model="Test", manufacturer=manufacturer)
+        self.assertEqual(str(car), car.model)
